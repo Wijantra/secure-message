55e64e05aaadeefcc690d47d77a8bc1eca6cff53
Ding <ding533@hotmail.com>
2021-02-23 17:52:48 +0700

    Change work factor to 13

--- src/main/java/ku/message/config/SecurityConfig.java
+++ src/main/java/ku/message/config/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder encoder() {
-        return new BCryptPasswordEncoder(12);
+        return new BCryptPasswordEncoder(13);
     }
 
 
